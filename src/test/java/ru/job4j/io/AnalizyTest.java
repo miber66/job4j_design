0f98e0199a3886f7ed3cc588baa67d299049ab4c
miber66 <markkhokhlyavin@gmail.com>
2022-09-15 11:09:48 +0500

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AnalizyTest {
+    @Test
+    void drop(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("server.log").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:55:01");
+            out.println("200 10:58:01");
+            out.println("400 10:59:01");
+            out.println("500 11:00:03");
+            out.println("300 11:03:01");
+        }
+        File target  = tempDir.resolve("unavailable.csv").toFile();
+        Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat("10:59:01;11:00:03;11:03:01;").isEqualTo(rsl.toString());
+    }
+}
